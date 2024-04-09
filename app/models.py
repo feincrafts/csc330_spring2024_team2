60cbfa24d7df6318cafc3210821f248833bdb48f
Cristian <cristiangaming5505@gmail.com>
2024-04-09 12:40:32 -0400

    modified models, added __repr__

--- app/models.py
+++ app/models.py
@@ -10,10 +10,15 @@ class User(db.Model):
     email = db.Column(db.String(64), index=True, unique=True, nullable=False)
     planner = db.relationship('Planner', backref='users', lazy='dynamic')
 
+    def __repr__(self):
+        return '{} {}'.format(self.username, self.email)
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, nullable=False)
     tasks = db.relationship('Task', backref='game', lazy=True)
+    
+    def __repr__(self):
+        return '{} {}'.format(self.name)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +26,9 @@ class Task(db.Model):
     complete = db.Column(db.Boolean, index=True, default=False)
     users = db.relationship('User', secondary='user_task', backref='tasks')
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
+    
+    def __repr__(self):
+        return '{} {}'.format(self.goal, self.complete)
 
 class Planner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,3 +42,6 @@ class Planner(db.Model):
             db.session.commit()
             return True
         return False
+    
+    def __repr__(self):
+        return '{} {}'.format(self)
