60d7c8146fef388785bba66eeef5890eed161a7d
merinanngodly <150188113+merinanngodly@users.noreply.github.com>
2024-04-18 11:55:38 -0400

    Update forms.py (2)

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,TextAreaField, DateField, RadioField, BooleanField, SelectField, SelectMultipleField
+from wtforms import StringField,PasswordField,SubmitField,TextAreaField, IntegerField, DateField, RadioField, BooleanField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, StopValidation
 from wtforms.widgets import CheckboxInput, ListWidget
 
